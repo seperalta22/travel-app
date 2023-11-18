4ef7c78b44a7f65f2b9a96554d4aaf8ae8a2561b
Sergio Peralta <seperalta88@gmail.com>
2023-11-18 10:50:58 -0300

    Update layout and remove unused files

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className=' flexBetween max-container padding-container relative z-30 py-5'>
+      <Link href='/'>
+        <Image src='/hilink-logo.svg' alt='logo' width={74} height={29} />
+      </Link>
+      <ul className='hidden h-full gap-12 lg:flex'></ul>
+    </nav>
+  );
+};
+export default Navbar;
