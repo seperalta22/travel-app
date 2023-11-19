a4ee10f74fed9cea0a2026599cd3c796cf7f9ea4
Sergio Peralta <seperalta88@gmail.com>
2023-11-19 15:40:25 -0300

    Add Campsite component and update Camp component

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,4 +1,67 @@
+import { PEOPLE_URL } from "@/constants";
+import Image from "next/image";
+
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+const Campsite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      <div className='flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10'>
+        <div className='flexCenter gap-4'>
+          <div className='rounded-full bg-green-50 p-4'>
+            <Image src='/folded-map.svg' alt='map' width={28} height={28} />
+          </div>
+          <div className='flex flex-col gap-1'>
+            <h4 className='bold-18 text-white'>{title}</h4>
+            <p className='regular-14 text-white'>{subtitle}</p>
+          </div>
+        </div>
+
+        <div className='flexCenter gap-6'>
+          <span className='flex -space-x-4 overflow-hidden'>
+            {PEOPLE_URL.map((url) => (
+              <Image
+                key={url}
+                src={url}
+                alt='person'
+                width={52}
+                height={52}
+                className='inline-block h-10 w-10 rounded-full'
+              />
+            ))}
+          </span>
+          <p className='bold-16 md:bold-20 text-white'>{peopleJoined}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Camp = () => {
-  return <div>Camp</div>;
+  return (
+    <section className='2xl:max-container relative flex flex-co py-10 lg:mb-10 lg:py-20 xl:mb-20 '>
+      <div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px] '>
+        <Campsite
+          backgroundImage='bg-bg-img-1'
+          title='Putuk Truno Camp Area'
+          subtitle='Pringen, Pasuruan'
+          peopleJoined='50+ Joined'
+        />
+        <Campsite />
+      </div>
+    </section>
+  );
 };
 export default Camp;
